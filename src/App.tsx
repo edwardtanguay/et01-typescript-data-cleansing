74ef8c381f7fd46b9dcfc19753be54fed42b98e0
Edward Tanguay (windows) <edward@tanguay.info>
2022-08-18 10:32:43 +0200

    final

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,41 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.scss';
-import books from './data/techBooksUnstructured.json';
+
+const url =
+	'https://edwardtanguay.netlify.app/share/techBooksUnstructured.json';
+
+interface IBook {
+	id: number;
+	title: string;
+	description: string;
+	language: string;
+	yearMonth: string;
+	numberInStock: number;
+}
 
 function App() {
+	const [books, setBooks] = useState<IBook[]>([]);
+
+	useEffect(() => {
+		(async () => {
+			const rawBooks = (await axios.get(url)).data;
+			const books: IBook[] = [];
+			rawBooks.forEach((rawBook:any) => {
+				const book: IBook = {
+					id: rawBook.id,
+					title: rawBook.title,
+					description: rawBook.description,
+					language: !rawBook.language ? 'english' : rawBook.language,
+					yearMonth: rawBook.yearMonth,
+					numberInStock: rawBook.numberInStock,
+				};
+				books.push(book);
+			});
+			setBooks(books);
+		})();
+	}, []);
+
 	return (
 		<div className="App">
 			<h1>TypeScript Data-Cleansing Example</h1>
