69b17abf68fd08bc32913926398acf28cb7490d7
Edward Tanguay (windows) <edward@tanguay.info>
2022-08-18 06:02:17 +0200

    all three

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import './App.scss';
 
 function App() {
-    return (
-        <div className="App">
-            <h1>TypeScript Data-Cleansing Example</h1>
-            <p>testing</p>
-        </div>
-    );
+
+	let age: number = 20;
+	let name: string = "Jan";
+	let isOnline: boolean = false;
+
+	age = "15";
+	name = 0;
+	isOnline = "true";
+
+
+
+	return (
+		<div className="App">
+			<h1>TypeScript Data-Cleansing Example</h1>
+			<p>testing</p>
+		</div>
+	);
 }
 
 export default App;
